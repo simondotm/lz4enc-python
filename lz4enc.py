c9468072244aacb1d6f5cdc4316cd95405b65c3a
simondotm <simondotm@users.noreply.github.com>
2019-01-19 12:56:37 +0000

    Latest version

--- lz4enc.py
+++ lz4enc.py
@@ -22,6 +22,7 @@
 #// SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 import struct
+import os
 
 
 
@@ -61,6 +62,7 @@ class SmallLZ4():
 #    obj.compress(getBytes, sendBytes, dictionary, useLegacyFormat);
 #  }
 #  /// version string
+  Version = "1.3"
 #  static const char* const getVersion()
 #  {
 #    return "1.3";
@@ -126,7 +128,12 @@ class SmallLZ4():
 
   
   #/// create new compressor (only invoked by lz4)
-  def __init__(self, newMaxChainLength = MaxChainLength):
+  def __init__(self, level = 9):
+    if (level >= 9):
+      newMaxChainLength = 65536  #// "unlimited" because search window contains only 2^16 bytes 
+    else:
+      newMaxChainLength = level
+
     self.maxChainLength = newMaxChainLength
     #// => no limit, but can be changed by setMaxChainLength
 
@@ -144,7 +151,7 @@ class SmallLZ4():
   # data - bytearray
   # pos, begin, end - int
   # previous - Distance list/array
-  def findLongestMatch(data, pos, begin, end, previous):
+  def findLongestMatch(self, data, pos, begin, end, previous):
   
     #/// return true, if the four bytes at data[a] and data[b] match
     def match4(a, b):
@@ -152,11 +159,11 @@ class SmallLZ4():
       lb = struct.unpack('>L', data[b:b+4])
       return a == b
   
-    result = Match()
+    result = self.Match()
     result.length = 1
 
     #// compression level: look only at the first n entries of the match chain
-    stepsLeft = maxChainLength
+    stepsLeft = self.maxChainLength
 
     #// pointer to position that is matched against everything in data
     #const unsigned char* const current = data + pos - begin
@@ -167,16 +174,16 @@ class SmallLZ4():
     stop    = current + end - pos
 
     #// get distance to previous match, abort if 0 => not existing
-    distance = previous[pos % PreviousSize]
+    distance = previous[pos % self.PreviousSize]
     totalDistance = 0
-    while (distance != NoPrevious):
+    while (distance != self.NoPrevious):
       #// too far back ?
       totalDistance += distance
-      if (totalDistance > MaxDistance):
+      if (totalDistance > self.MaxDistance):
         break
 
       #// prepare next position
-      distance = previous[(pos - totalDistance) % PreviousSize]
+      distance = previous[(pos - totalDistance) % self.PreviousSize]
       #// stop searching on lower compression levels
       stepsLeft -= 1
       if (stepsLeft <= 0):
@@ -254,7 +261,7 @@ class SmallLZ4():
     literalsTo   = 0 #// point beyond last literal of the current run
     #// walk through the whole block
     #for (size_t offset = 0; offset < matches.size(); ): #// increment inside of loop
-    for offset in xrange(len(matches)):
+    for offset in range(len(matches)):
 
       #// get best cost-weighted match
       #Match match = matches[offset]
@@ -299,7 +306,10 @@ class SmallLZ4():
         else:
           token |= 15
 
-      result.append( struct.pack('B', token) )
+      #print(token)
+      #print(type(token))
+      result.append( token ) #struct.pack('B', token) )
+      #print(result)
 
       #// >= 15 literals ? (extra bytes to store length)
       if (numLiterals >= 15):
@@ -308,11 +318,13 @@ class SmallLZ4():
         numLiterals -= 15
         #// emit 255 until remainder is below 255
         while (numLiterals >= 255):       
-          result.append( struct.pack('B', 255) )
+          #result.append( struct.pack('B', 255) )
+          result.append(255)
           numLiterals -= 255
         
         #// and the last byte (can be zero, too)
-        result.append( struct.pack('B', numLiterals) )
+        #result.append( struct.pack('B', numLiterals) )
+        result.append(numLiterals)
       
       #// copy literals
       if (literalsFrom != literalsTo):
@@ -327,19 +339,23 @@ class SmallLZ4():
         break
 
       #// distance stored in 16 bits / little endian
-      result.append( struct.pack('B', match.distance & 0xFF) )
-      result.append( struct.pack('B', (match.distance >> 8) & 0xFF) )
+      #result.append( struct.pack('B', match.distance & 0xFF) )
+      result.append( match.distance & 0xFF )
+      #result.append( struct.pack('B', (match.distance >> 8) & 0xFF) )
+      result.append( (match.distance >> 8) & 0xFF )
       #// >= 15+4 bytes matched (4 is implied because it's the minimum match length)
       if (matchLength >= 15):
         #// 15 is already encoded in token
         matchLength -= 15
         #// emit 255 until remainder is below 255
         while (matchLength >= 255):
-          result.append( struct.pack('B', 255) )
+          #result.append( struct.pack('B', 255) )
+          result.append(255)
           matchLength -= 255
         
         #// and the last byte (can be zero, too)
-        result.append( struct.pack('B', matchLength) )
+        #result.append( struct.pack('B', matchLength) )
+        result.append(matchLength)
       
     
     return result
@@ -360,7 +376,7 @@ class SmallLZ4():
     posLastMatch = blockEnd
 
     #for (int i = (int)blockEnd - (1 + BlockEndLiterals); i >= 0; i--) #// ignore the last 5 bytes, they are always literals
-    for i in xrange(blockEnd - (1 + BlockEndLiterals), 0, -1 ):
+    for i in range(blockEnd - (1 + self.BlockEndLiterals), 0, -1 ):
 
       #// watch out for long literal strings that need extra bytes
       numLiterals = posLastMatch - i
@@ -377,12 +393,12 @@ class SmallLZ4():
       match = matches[i]    
       
       #// match must not cross block borders
-      if (match.isMatch() and i + match.length + BlockEndLiterals > blockEnd):
-        match.length = blockEnd - (i + BlockEndLiterals)
+      if (match.isMatch() and i + match.length + self.BlockEndLiterals > blockEnd):
+        match.length = blockEnd - (i + self.BlockEndLiterals)
 
       #// try all match lengths (first short ones)
       #for (Length length = MinMatch; length <= match.length; length++):
-      for length in xrange(MinMatch, match.length+1):
+      for length in range(self.MinMatch, match.length+1):
       
         #// token (1 byte) + offset (2 bytes)
         currentCost = cost[i + length] + 1 + 2
@@ -411,7 +427,7 @@ class SmallLZ4():
           #// on the other hand, literals are faster than short matches as well (assuming same cost)
         
         #// workaround: very long self-referencing matches can slow down the program A LOT
-        if (match.distance == 1 and match.length >= MaxSameLetter):
+        if (match.distance == 1 and match.length >= self.MaxSameLetter):
         
           #// assume that longest match is always the best match
           #// however, this assumption might not be optimal
@@ -421,7 +437,7 @@ class SmallLZ4():
         
       
       #// remember position of last match to detect number of consecutive literals
-      if (bestLength >= MinMatch):
+      if (bestLength >= self.MinMatch):
         posLastMatch = i
 
       #// store lowest cost so far
@@ -429,7 +445,7 @@ class SmallLZ4():
       #// and adjust best match
       matches[i].length = bestLength
       if (bestLength == 1):
-        matches[i].distance = NoPrevious
+        matches[i].distance = self.NoPrevious
 
       #// note: if bestLength is smaller than the previous matches[i].length then there might be a closer match
       #//       which could be more cache-friendly (=> faster decoding)
@@ -513,9 +529,12 @@ class SmallLZ4():
       #const unsigned char* dataBlock = NULL;
       # dataBlock is an offset now - see below
 
+
       #// prepend dictionary
       if (parseDictionary):
-      
+
+        print(" Loading Dictionary...")
+
         #// prepend exactly 64k
         MaxDictionary = 65536
         if (len(dictionary) < MaxDictionary):
@@ -537,6 +556,8 @@ class SmallLZ4():
       else:
         maxBlockSize = self.MaxBlockSize
 
+
+
       while (numRead - nextBlock < maxBlockSize):
       
         #// buffer can be significantly smaller than MaxBlockSize, that's the only reason for this while-block
@@ -554,6 +575,8 @@ class SmallLZ4():
       if (nextBlock == numRead):
         break
 
+      print(" Processing Block... " + str(numRead>>10) + "Kb, (maxBlockSize=" + str(maxBlockSize>>10) + "Kb, windowSize=" + str(self.MaxDistance>>10) + "Kb)")
+
       #// determine block borders
       lastBlock  = nextBlock
       nextBlock += maxBlockSize
@@ -569,6 +592,7 @@ class SmallLZ4():
       blockSize = nextBlock - lastBlock
 
       #// ==================== full match finder ====================
+      print("  Finding matches...")
       #// greedy mode is much faster but produces larger output
       isGreedy = (self.maxChainLength <= self.ShortChainsGreedy)
       #// lazy evaluation: if there is a (match, then try running match finder on next position, too, but not after that
@@ -595,11 +619,11 @@ class SmallLZ4():
         lookback = 0
   
       #std::vector<Match> matches(blockSize);
-      matches = [ self.Match() for i in xrange(blockSize) ]
+      matches = [ self.Match() for i in range(blockSize) ]
 
       #// find longest matches for each position
       #for (int i = lookback; i < (int)blockSize; i++)
-      for i in xrange(lookback, blockSize):
+      for i in range(lookback, blockSize):
         #// no matches at the end of the block (or matching disabled by command-line option -0 )
         if (i + self.BlockEndNoMatch > blockSize or uncompressed):
           continue
@@ -611,7 +635,7 @@ class SmallLZ4():
           prevMatch = matches[i - 1]
           
           #// predecessor had the same match ?
-          if (prevMatch.distance == 1 and prevMatch.length > MaxSameLetter): #// TODO: handle very long self-referencing matches          
+          if (prevMatch.distance == 1 and prevMatch.length > self.MaxSameLetter): #// TODO: handle very long self-referencing matches          
             #// just copy predecessor without further (expensive) optimizations
             prevMatch.length -= 1
             matches[i] = prevMatch
@@ -620,7 +644,7 @@ class SmallLZ4():
         def getLong(buffer, offset):
           end = offset + 4
           buf = buffer[offset:end]
-          four = struct.unpack('>L', buf)
+          four = struct.unpack('>L', buf)[0]
           return four
 
         #// read next four bytes
@@ -637,20 +661,20 @@ class SmallLZ4():
         lastHash[hash] = i + lastBlock
         
         #// remember: i could be negative, too
-        prevIndex = (i + PreviousSize) % PreviousSize
+        prevIndex = (i + self.PreviousSize) % self.PreviousSize
         
         #// no predecessor or too far away ?
         distance = i + lastBlock - last
-        if (last == NoLastHash or distance > MaxDistance):
-          previousHash[prevIndex] = NoPrevious
-          previousExact[prevIndex] = NoPrevious
+        if (last == NoLastHash or distance > self.MaxDistance):
+          previousHash[prevIndex] = self.NoPrevious
+          previousExact[prevIndex] = self.NoPrevious
           continue
         
         #// build hash chain, i.e. store distance to last match
         previousHash[prevIndex] = distance
 
         #// skip pseudo-matches (hash collisions) and build a second chain where the first four bytes must match exactly
-        while (distance != NoPrevious):
+        while (distance != self.NoPrevious):
           #uint32_t curFour = *(uint32_t*)(&data[last - dataZero]); #// may be in the previous block, too
           curFour = getLong(data, last - dataZero)  #// may be in the previous block, too
 
@@ -666,26 +690,26 @@ class SmallLZ4():
             break
           
           #// try next pseudo-match
-          next = previousHash[last % PreviousSize]
+          next = previousHash[last % self.PreviousSize]
 
           #// pointing to outdated hash chain entry ?
           distance += next
 
-          if (distance > MaxDistance):
-            previousHash[last % PreviousSize] = NoPrevious
-            distance = NoPrevious
+          if (distance > self.MaxDistance):
+            previousHash[last % self.PreviousSize] = self.NoPrevious
+            distance = self.NoPrevious
             break
           
           #// closest match is out of range ?
           last -= next
-          if (next == NoPrevious or last < dataZero):
-            distance = NoPrevious
+          if (next == self.NoPrevious or last < dataZero):
+            distance = self.NoPrevious
             break
           
         
         #// no match at all ?
-        if (distance == NoPrevious):
-          previousExact[prevIndex] = NoPrevious
+        if (distance == self.NoPrevious):
+          previousExact[prevIndex] = self.NoPrevious
           continue
         
         #// store distance to previous match
@@ -704,8 +728,10 @@ class SmallLZ4():
           lazyEvaluation = False
         
         #// and look for longest match
+        #print(" Finding longest matches...")
+
         #Match longest = findLongestMatch(&data[0], i + lastBlock, dataZero, nextBlock - BlockEndLiterals + 1, &previousExact[0]);
-        longest = findLongestMatch(data, i + lastBlock, dataZero, nextBlock - BlockEndLiterals + 1, previousExact)
+        longest = self.findLongestMatch(data, i + lastBlock, dataZero, nextBlock - self.BlockEndLiterals + 1, previousExact)
         matches[i] = longest
 
         #// no match finding needed for the next few bytes in greedy/lazy mode
@@ -718,16 +744,20 @@ class SmallLZ4():
       parseDictionary = False
       
       #// ==================== estimate costs (number of compressed bytes) ====================
+      print("  Estimating costs...")
+
       #// not needed in greedy mode and/or very short blocks
       if (len(matches) > self.BlockEndNoMatch and self.maxChainLength > self.ShortChainsGreedy):
-        estimateCosts(matches)
+        self.estimateCosts(matches)
 
       #// ==================== select best matches ====================
+      print("  Selecting best matches...")
+      
       #std::vector<unsigned char> block;
       block = bytearray()
       if (not uncompressed):
         #block = selectBestMatches(matches, &data[lastBlock - dataZero]);
-        block = selectBestMatches(matches, data, lastBlock - dataZero )
+        block = self.selectBestMatches(matches, data, lastBlock - dataZero )
       
       #// ==================== output ====================
       #// automatically decide whether compressed or uncompressed
@@ -735,6 +765,12 @@ class SmallLZ4():
 
       #// did compression do harm ?
       useCompression   = len(block) < uncompressedSize and not uncompressed
+
+      print(" Writing output block - uncompressed (" + str(uncompressedSize) + "), compressed (" + str(len(block)) + ") ...")
+      if useCompression:
+        print("  Compressed data selected for this block.")
+      else:
+        print("  Uncompressed data selected for this block.")
 
       #// legacy format is always compressed
       if useLegacyFormat:
@@ -780,12 +816,12 @@ class SmallLZ4():
 
         #// clear hash tables
         #for (size_t i = 0; i < previousHash.size(); i++)
-        for i in xrange(len(previousHash)):
+        for i in range(len(previousHash)):
           previousHash[i] = NoPrevious
           previousExact[i] = NoPrevious
 
         #for (size_t i = 0; i < lastHash.size(); i++)
-        for i in xrange(len(lastHash)):
+        for i in range(len(lastHash)):
           lastHash[i] = NoLastHash
     
       else:
@@ -807,13 +843,65 @@ class SmallLZ4():
 # main()
 #-------------------------
 
-compressor = SmallLZ4()
 
-file_in = open("test.vgm", 'rb')
-file_out = open("lz.bin", 'wb')
+print("smallz4 V" + str(SmallLZ4.Version) + ": compressor with optimal parsing, fully compatible with LZ4 by Yann Collet (see https://lz4.org)")
+print("")
+print("Basic usage:")
+print("  smallz4 [flags] [input] [output]")
+print("")
+print("This program writes to STDOUT if output isn't specified")
+print("and reads from STDIN if input isn't specified, either.")
+print("")
+print("Examples:")
+print("  smallz4   < abc.txt > abc.txt.lz4    # use STDIN and STDOUT")
+print("  smallz4     abc.txt > abc.txt.lz4    # read from file and write to STDOUT")
+print("  smallz4     abc.txt   abc.txt.lz4    # read from and write to file")
+print("  cat abc.txt | smalLZ4 - abc.txt.lz4  # read from STDIN and write to file")
+print("  smallz4 -6  abc.txt   abc.txt.lz4    # compression level 6 (instead of default 9)")
+print("  smallz4 -f  abc.txt   abc.txt.lz4    # overwrite an existing file")
+print("  smallz4 -f7 abc.txt   abc.txt.lz4    # compression level 7 and overwrite an existing file")
+print("")
+print("Flags:")
+print("  -0, -1 ... -9   Set compression level, default: 9 (see below)")
+print("  -h              Display this help message")
+print("  -f              Overwrite an existing file")
+print("  -l              Use LZ4 legacy file format")
+print("  -D [FILE]       Load dictionary")
+print("")
+print("Compression levels:")
+print(" -0               No compression")
+print(" -1 ... -" + str(SmallLZ4.ShortChainsGreedy) +"        Greedy search, check 1 to " + str(SmallLZ4.ShortChainsGreedy) + " matches")
+print(" -" + str(SmallLZ4.ShortChainsGreedy+1) + " ... -8        Lazy matching with optimal parsing, check " + str(SmallLZ4.ShortChainsGreedy+1) + " to 8 matches")
+print(" -9               Optimal parsing, check all possible matches (default)")
+print("")
+print("Written in 2016-2018 by Stephan Brumme https://create.stephan-brumme.com/smallz4/")
+print("")
 
+# Pass compression level to compressor
+#  "Compression levels:\n"
+#    " -0               No compression\n"
+#    " -1 ... -%d        Greedy search, check 1 to %d matches\n"
+#    " -%d ... -8        Lazy matching with optimal parsing, check %d to 8 matches\n"
+#    " -9               Optimal parsing, check all possible matches (default)\n"
+
+
+compression_level = 8
+src = "test.vgm"
+dst = src + ".lz4"
+
+print("Compressing file '" + src + "' to '" + dst + "', using compression level " + str(compression_level) )
+
+compressor = SmallLZ4(compression_level)
+file_in = open(src, 'rb')
+file_out = open(dst, 'wb')
 compressor.compress(file_in, file_out, bytearray(), False)
-
 file_in.close()
 file_out.close()
 
+src_size = os.path.getsize(src)
+dst_size = os.path.getsize(dst)
+ratio = 100 - (int)((dst_size / src_size)*100)
+
+print(" Input file " + str(src_size) + " bytes, Output file " + str(dst_size) + ", (" + str(ratio) + "% compression)" )
+
+print("Complete.")
